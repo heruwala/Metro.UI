dfcc4232b7bdcdb3b7b0041fd472653af642c9b4
Dhaval Heruwala <heruwala@gmail.com>
2018-11-10 10:37:20 -0500

    removed comments

--- js/Trailhead.js
+++ js/Trailhead.js
@@ -1,23 +1,11 @@
 var allData = [];
 window.onload = function(){
-    //var users = ["gidich", "dhaval", "nathanielm", "raymunyan", "mikecorsi", "00550000007G8A3AAK"];
+
+    // refresh every 30 seconds
     setInterval('refreshPage()', 30000);
 
     var tempUsers = JSON.parse(getUsers());
     var trailHeadUsers = tempUsers.user;
-    /*
-    var trailHeadUsers = [ 
-        { "username" : "gidich" , "name" : "Patrick Gidich" } ,
-        { "username" : "dhaval" , "name" : "Dhaval Heruwala" } ,
-        { "username" : "nathanielm" , "name" : "Nathan Mccarthy" } ,
-        { "username" : "raymunyan" , "name" : "Ray Munyan" } ,
-        { "username" : "mikecorsi" , "name" : "Mike Corsi" } ,
-        { "username" : "00550000007G8A3AAK" , "name" : "Valeri Gorbochov" } 
-    ];
-    */
-    //alert(tempUsers.user);
-
-    //alert(trailHeadUsers);
 
     trailHeadUsers.forEach(user => {
         GetTrailheadAchievements(user); 
@@ -33,15 +21,8 @@ window.onload = function(){
           sorted.sort(function (b, a) {
              return a.Points - b.Points;
           });
-          console.log(sorted);
-          /*
-          jQuery.each( sorted, function( i, val ) {
-            ///alert( i + ": " + val.user );
-            $( "#leader" + i ).append( document.createTextNode( (i + 1) + " - " + val.user ) );
-          });
-          */
 
-          // Top 6 only
+          // Display Top 6 only
           for (i = 0; i < 6; i++) { 
             $( "#leader" + i ).append( document.createTextNode( (i + 1) + " - " + sorted[i].name ) );
         }
@@ -50,6 +31,14 @@ window.onload = function(){
 
 
 };
+
+function getUsers() {
+    return $.ajax({
+        type: "GET",
+        url: "https://script.google.com/macros/s/AKfycbx2Hck1djplDYpaKuF0OgonDwsaD2m43FVreLaQ8TeA6Sx1arHd/exec",
+        async: false
+    }).responseText;
+}
 
 function GetTrailheadAchievements(user) {
     $.ajax({
@@ -61,20 +50,10 @@ function GetTrailheadAchievements(user) {
         }
     }).done(function (data) {
         data.name = user.name;
-        console.log(data);
-        //alert(data.Points);
         allData.push(data);       
     });
 }
 
-
-function getUsers() {
-    return $.ajax({
-        type: "GET",
-        url: "https://script.google.com/macros/s/AKfycbx2Hck1djplDYpaKuF0OgonDwsaD2m43FVreLaQ8TeA6Sx1arHd/exec",
-        async: false
-    }).responseText;
-}
 
 function refreshPage() { 
     var today = new Date().getHours();
